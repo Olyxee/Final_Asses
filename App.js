02e7edc923101203a411f2c5d9d1d2f1fc420af7
Olyxee <innolethabo98@gmail.com>
2025-11-04 15:37:56 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { AuthProvider } from "./src/context/AuthContext";
+import AppNavigator from "./src/navigation/AppNavigator";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <AppNavigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </AuthProvider>
+  );
+}
